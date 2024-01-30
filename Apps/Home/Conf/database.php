58153c7c7e7212487ac3f7d9aa65eb2bdf3f79af
roadway01 <roadway01@users.noreply.github.com>
2024-01-30 07:41:18 +0000

    更新

---
+++ Apps/Home/Conf/database.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE' => 'mysql',// 数据库类型
+	'DB_HOST' => '121.40.25.252',// 服务器地址 192.168.44.200
+	'DB_NAME' => 'gold_lock',// 数据库名
+	'DB_USER' => 'test1',// 用户名
+	'DB_PWD' => 'TEST1&cffex.com.cn',// 密码
+	'DB_PORT' => '3306',// 端口
+	'DB_PREFIX' => '',// 数据库表前缀
+	'DB_CHARSET' => 'utf8',// 字符集
+);
